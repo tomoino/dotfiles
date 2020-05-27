cc8f72d46cd459afcfb483275b0f6dc93e51977a
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-05-27 21:46:24 +0900

    [update]config.fish: add JDK_HOME
    JDKのパスを追加

--- fish/config.fish
+++ fish/config.fish
@@ -1,4 +1,5 @@
-set PATH $HOME/bin $HOME/.local/bin $PATH
+set JDK_HOME "/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
+set PATH $JDK_HOME $HOME/bin $HOME/.local/bin $PATH
 
 export DISPLAY=:0.0
 source ~/.config/fish/alias.fish
