dbf48fefb570e679657f4167bc1fedb60ba4002d
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-06-09 11:20:28 +0900

    [update]fish for WSL2
    WSL2でもGUIが表示できるように変更

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,7 @@
 set JDK_HOME "/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
 set PATH $JDK_HOME $HOME/bin $HOME/.local/bin $PATH
+set -e LIBGL_ALWAYS_INDIRECT
 
-export DISPLAY=:0.0
+export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
 source ~/.config/fish/alias.fish
 source ~/.config/fish/function.fish
