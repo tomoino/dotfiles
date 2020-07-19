660aac13666222c521cbf0d7a0fbf7660f6b7d8e
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-07-19 17:33:07 +0900

    [update]config
    GoのPATHを通す

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,6 @@
 set JDK_HOME "/usr/lib/jvm/java-11-openjdk-amd64/bin/java"
-set PATH $JDK_HOME $HOME/bin $HOME/.local/bin $PATH
+set GO_PATH "/usr/local/go/bin"
+set PATH $JDK_HOME $GO_PATH $HOME/bin $HOME/.local/bin $PATH 
 set -e LIBGL_ALWAYS_INDIRECT
 
 export DISPLAY=(cat /etc/resolv.conf | grep nameserver | awk '{print $2}'):0.0
