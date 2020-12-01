7099b6653cd10054353f896b6ab88996c81e3255
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2020-12-01 14:01:46 +0900

    [update]init_mac.sh
    fish設定追加

--- init_mac.sh
+++ init_mac.sh
@@ -33,21 +33,19 @@ if !(type brew > /dev/null 2>&1); then
         sudo mkdir homebrew
         sudo chown $USER homebrew
         curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+
+        # M1 の場合には、Rosetta2 版の brew を brew_x86 という名前で実行できるようにしておく
+        cd /usr/local/bin
+        ln -s brew brew_x86
     fi
 
 #====================================================================
 # sshの導入
 #====================================================================
-    # mkdir ~/.ssh
-    # cd ~/.ssh
     ssh-keygen -t rsa
-    # cd ~
-    # yes | sudo apt-get install xclip
-    # cat ~/.ssh/id_rsa.pub | xclip -selection clipboard
     pbcopy < ~/.ssh/id_rsa.pub
-    # yes | sudo apt-get install xdg-utils
-    # xdg-open https://github.com
-    read -p "Upload SSH public key to GitHub!"
+    read -p "Upload SSH public key to GitHub!" # Github > settings > SSH and GPG keys > New SSH Key から追加
+    ssh -T git@github.com # 確認
     cd ~
     git clone git@github.com:tomoino/dotfiles.git
 fi
@@ -55,13 +53,21 @@ fi
 # fish
 #====================================================================
 if !(type fish > /dev/null 2>&1); then
-    yes | sudo apt-add-repository ppa:fish-shell/release-3
-    yes | sudo apt-get update
-    yes | sudo apt-get install -y fish
+    if is_arm; then
+        /opt/homebrew/bin/brew install fish # 一部armに未対応だったため、実際にはサイトから直接 DL & インストール
+    else
+        brew install fish
+    fi   
     # curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
-    yes | sudo apt-get install fonts-powerline
+    echo /usr/local/bin/fish | sudo tee -a /etc/shells
+    chsh -s /usr/local/bin/fish
+    
+    git clone https://github.com/powerline/fonts.git
+    cd fonts
+    ./install.sh # terminalのフォント変更を忘れずに。
+    # yes | sudo apt-get install fonts-powerline
     # fisher omf/theme-agnoster
-    echo exec fish > ~/.bashrc
+    # echo exec fish > ~/.bashrc
     mkdir ~/.config/fish
     ln -s ~/dotfiles/fish/config.fish ~/.config/fish/config.fish
     ln -s ~/dotfiles/fish/alias.fish ~/.config/fish/alias.fish
@@ -69,6 +75,7 @@ if !(type fish > /dev/null 2>&1); then
     mkdir ~/.config/fish/functions
     ln -s ~/dotfiles/fish/fish_prompt.fish ~/.config/fish/functions/fish_prompt.fish
     mkdir ~/.config/fish/completions/
+    fish
 fi
 
-fish ~/dotfiles/setup_ubuntu.fish
+# fish ~/dotfiles/setup_ubuntu.fish
