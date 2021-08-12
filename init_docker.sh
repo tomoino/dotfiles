5b9686f4a88c98005f823a45e7eacc8105be587b
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-08-12 16:11:32 +0900

    [update] init docker

--- init_docker.sh
+++ init_docker.sh
@@ -36,7 +36,5 @@ set fish_color_user brgreen
 set fish_color_valid_path --underline
 
 # exa
-curl https://sh.rustup.rs -sSf | sh -y
-wget -q https://github.com/ogham/exa/releases/download/v0.9.0/exa-linux-x86_64-0.9.0.zip
-unzip exa-linux-x86_64-0.9.0.zip
-sudo mv exa-linux-x86_64 /usr/local/bin/exa
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+cargo install exa
