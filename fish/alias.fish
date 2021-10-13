e8c6cf169e1eb7f4ee6eccd6155bc09da2e8104d
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-10-13 15:52:14 +0900

    [update] remove alias

--- fish/alias.fish
+++ fish/alias.fish
@@ -51,7 +51,6 @@ alias dcd 'docker-compose down'
 alias dbs 'sh docker/build.sh'
 alias drs 'sh docker/run.sh'
 alias des 'sh docker/exec.sh'
-alias dres 'sh docker/run.sh && sh docker/exec.sh'
 alias dss 'sh docker/setup.sh'
 
 # Google Cloud Platform
