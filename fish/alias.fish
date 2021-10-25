0e0d81b7641f56a894dd516b700374a9216d9b26
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-10-25 13:17:11 +0900

    [add] prune alias

--- fish/alias.fish
+++ fish/alias.fish
@@ -43,6 +43,7 @@ alias dds 'sudo service docker start'
 alias dstp 'docker stop'
 alias drm 'docker rm'
 alias drmi 'docker rmi'
+alias dpr 'docker system prune -a --filter "until=24h"'
 alias dcp 'docker container prune'
 
 alias dcu 'docker-compose up -d'
