b83646fdb0bd83b5455c5595fc05e61a00dfd1c0
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-11-16 18:56:43 +0900

    [update] zsh

--- zsh/install.sh
+++ zsh/install.sh
@@ -2,8 +2,8 @@
 sudo apt install zsh
 zsh
 cd ~
-git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+# git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 setopt EXTENDED_GLOB
-for rcfile in ~/.zprezto/runcoms/^README.md(.N); do
+for rcfile in ~/dotfiles/zsh/.zprezto/^README.md(.N); do
   ln -s "$rcfile" ~/.${rcfile:t}
 done
