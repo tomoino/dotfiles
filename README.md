8402d1ce7f0b923c11389d533aca062f1f9b7ed1
Tomohiro Inoue <55827264+tomoino@users.noreply.github.com>
2021-11-21 15:54:08 +0900

    [update] zsh

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # dotfiles
+Fonts recommendation: [CascadiaCode Nerd Font](https://github.com/AaronFriel/nerd-fonts/releases/tag/v1.2.0)
--- zsh/install.sh
+++ zsh/install.sh
@@ -2,7 +2,7 @@
 sudo apt install zsh
 zsh
 cd ~
-# git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
+git clone --recursive https://github.com/sorin-ionescu/prezto.git ~/.zprezto
 setopt EXTENDED_GLOB
 for rcfile in ~/dotfiles/zsh/.zprezto/^README.md(.N); do
   ln -s "$rcfile" ~/.${rcfile:t}
